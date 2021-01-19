ea44bafa6aba3e236b769b33cbbaee1b0b1c82b7
Gabriel-Martinez0 <gabriel.martinez@etu.umontpellier.fr>
2021-01-19 18:38:12 +0100

    anime bato fini

--- UnitySurprise/Assets/Dylan/Scripts/CameraMovement.cs
+++ UnitySurprise/Assets/Dylan/Scripts/CameraMovement.cs
@@ -10,6 +10,9 @@ public class CameraMovement : MonoBehaviour
     public float vitesse_perso;
     public string forme;
 
+    private float xglisse;
+    private float yglisse;
+
 
     void Start(){
 
@@ -21,10 +24,19 @@ public class CameraMovement : MonoBehaviour
         float xmove = Input.GetAxisRaw("Horizontal");
         float ymove = Input.GetAxisRaw("Vertical");
 
+        if (xmove != 0 || ymove != 0){
+            xglisse = xmove;
+            yglisse = ymove;
+        }
+
         switch(forme){
 
             case "bateau":
-                rb.AddForce(new Vector2(xmove, ymove) * vitesse_bateau);
+                rb.velocity = new Vector2(xglisse, yglisse) * vitesse_bateau;
+                break;
+
+            case "bateau_rame":
+                rb.velocity = new Vector2(xglisse, yglisse) * vitesse_bateau;
                 break;
 
             default :
--- UnitySurprise/Assets/Dylan/Scripts/PlayerMovement.cs
+++ UnitySurprise/Assets/Dylan/Scripts/PlayerMovement.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator animator;
 
     public float vitesse_bateau;
     public float vitesse_perso;
     public string forme;
+    public int level;
+
+    private float xglisse;
+    private float yglisse;
 
 
     void Start(){
@@ -21,11 +26,53 @@ public class PlayerMovement : MonoBehaviour
         float xmove = Input.GetAxisRaw("Horizontal");
         float ymove = Input.GetAxisRaw("Vertical");
 
+        if (xmove != 0 || ymove != 0){
+        	xglisse = xmove;
+        	yglisse = ymove;
+        }
+
+        
+
         switch(forme){
 
-            case "bateau":
-                rb.AddForce(new Vector2(xmove, ymove) * vitesse_bateau);
-                break;
+        	case "bateau":
+
+        		if(yglisse == 1 && xglisse == 1){
+        			rb.rotation = 315;
+            	}
+            	else if (yglisse == 1 && xglisse == -1){
+            		rb.rotation = 45;
+            	}
+            	else if (yglisse == -1 && xglisse == 1){
+            		rb.rotation = 225;
+            	}
+            	else if (yglisse == -1 && xglisse == -1){
+            		rb.rotation = 135;
+            	}
+            	else if(yglisse == 1){
+            		rb.rotation = 0;
+            	}
+            	else if (yglisse == -1){
+            		rb.rotation = 180;
+            	}
+            	else if (xglisse == -1){
+            		rb.rotation = 90;
+            	}
+            	else {
+            		rb.rotation = 270;
+            	}
+
+
+
+		        if(level > 0)
+		        {
+		        	animator.SetInteger("level", level);
+		        	animator.SetFloat("speed", Mathf.Abs(xglisse)+Mathf.Abs(yglisse));
+		        }
+
+		        rb.velocity = new Vector2(xglisse, yglisse) * vitesse_perso;
+	        	break;
+
 
             default :
                 rb.velocity = new Vector2(xmove, ymove) * vitesse_perso;
