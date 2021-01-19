4cb7d23337113b16645612bae063c53fdad19828
Gabriel-Martinez0 <gabriel.martinez@etu.umontpellier.fr>
2021-01-19 15:36:28 +0100

    deplacement final

--- UnitySurprise/Assets/Dylan/Scripts/PlayerMovement.cs
+++ UnitySurprise/Assets/Dylan/Scripts/PlayerMovement.cs
@@ -4,32 +4,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
+	public Rigidbody2D rb;
 
-    public float vitesse_bateau;
-    public float vitesse_perso;
-    public string forme;
+	public float vitesse_bateau;
+	public float vitesse_bateau_max;
+	public float vitesse_perso;
+
+	public string forme;
+
+	private float xglisse;
+	private float yglisse;
 
 
-    void Start(){
+	void Start(){
 
-    }
+	}
 
-    void Update()
-    {
+	void Update()
+	{
 
-        float xmove = Input.GetAxisRaw("Horizontal");
-        float ymove = Input.GetAxisRaw("Vertical");
+		float xmove = Input.GetAxisRaw("Horizontal");
+		float ymove = Input.GetAxisRaw("Vertical");
 
-        switch(forme){
+		if(xmove != 0 || ymove != 0){
+			xglisse = xmove;
+			yglisse = ymove;
+		}
+		
 
-            case "bateau":
-                rb.AddForce(new Vector2(xmove, ymove) * vitesse_bateau);
-                break;
 
-            default :
-                rb.velocity = new Vector2(xmove, ymove) * vitesse_perso;
-                break;
-        }
-    }
+
+		switch(forme){
+			case "bateau":
+				rb.velocity = new Vector2(xglisse, yglisse) * vitesse_perso;
+				break;
+
+			default :
+				rb.velocity = new Vector2(xmove, ymove) * vitesse_perso;
+				break;
+		}
+	}
+
+	void setForme(string _forme){
+		this.forme = _forme;
+	}
 }
